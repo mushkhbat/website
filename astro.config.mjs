a906105d147bc7e3d50246a23ccb77fc03a6f381
x7md <x7mdNet@gmail.com>
2022-09-26 03:31:49 +0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@ import { defineConfig } from 'astro/config';
 import rehypeImg from "rehype-figure-for-img";
 import remarkUnwrap from "remark-unwrap-images";
 import rehypeWrap from "rehype-wrap-all"
-import remarkGfm from 'remark-gfm';
-import remarkRehype from 'remark-rehype';
-import {remarkExtendedTable, extendedTableHandlers} from 'remark-extended-table';
 import react from "@astrojs/react";
 import sitemap from '@astrojs/sitemap';
+import rehypeRewrite from 'rehype-rewrite';
+
 
 import mdx from "@astrojs/mdx";
 
@@ -14,14 +13,26 @@ import mdx from "@astrojs/mdx";
 export default defineConfig({
   integrations: [sitemap(), react(), mdx(
     {remarkPlugins: {extends: [remarkUnwrap]},
-      rehypePlugins: [rehypeImg, [rehypeWrap, {
+      rehypePlugins: [rehypeImg,
+      [rehypeWrap, {
         selector: 'table',
         wrapper: 'figure'
-      }], 
+      }],
       [rehypeWrap, {
-        selector: 'img',
-        wrapper: 'picture.hi'
-      }]]
+        selector: 'img[src$="#blur"]',
+        wrapper: 'picture#blur'
+      }],
+      [rehypeRewrite, {
+          rewrite: (node) => {
+            if(node.tagName == 'img'){
+              Object.assign(node.properties, {
+                hight: '720',
+                width: '1280'
+              })
+            }
+          }
+        }
+      ]]
     }
   )],
   site: "https://www.mushkhbat.com/"
