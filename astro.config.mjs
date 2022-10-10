71e3a74c2c527d0cbc65c72ed56586dfe13fc1be
x7md <x7mdNet@gmail.com>
2022-10-10 16:30:23 +0300

    remove script ads

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ export default defineConfig({
           rewrite: (node) => {
             if(node.tagName == 'img'){
               Object.assign(node.properties, {
-                hight: '720',
+                height: '720',
                 width: '1280'
               })
             }
